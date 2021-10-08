ce6e031686c1e5a959dc1e72f8efbc247aed809f
Lee Berny Gifwandi <leesmacbookpro@macbook-pro.myfiosgateway.com>
2021-10-07 22:17:04 -0400

    updating code for heroku

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 
+Food Menu
+-------------------------------------------------------------------------------
+
+It's an app menu which an admin can create,read,update and delete data.
+
+Technologies Used
+-------------------------------------------------------------------------------
+
+- Ejs
+- CSS
+- JavaScript
+- Express
+-mongodb
+- Hosted on Heroku
+
+Screenshot
+-------------------------------------------------------------------------------
+![App!](images/App.jpg)
+![Wireframe!](images/Wireframe.jpg)
+
+
+Getting Started
+-------------------------------------------------------------------------------
+ [click here](https://vercel.com/nypdapi/project-one) to see the deployed app!
+
+Future Enhancements
+-------------------------------------------------------------------------------
+I want to be able to make the app display the complaints description when a user type in the number of recent complaints.
+
 
 
 
--- server.js
+++ server.js
@@ -12,16 +12,19 @@ const app = express();
 const methodOverride = require('method-override');
 
 
-// =======================
-//     MIDDLEWARE
-// =======================
+//___________________
+//Middleware
+//___________________
 
-// Body parser middleware: give us acces to req.body
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+//use public folder for static assets
+app.use(express.static('public'));
 
-// static files
-app.use(express.static('public'))
+// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
+app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
+app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
+
+//use method override
+app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 
 // =======================
 //    CONTROLLERS
@@ -29,15 +32,18 @@ app.use(express.static('public'))
 const menuController = require('./controllers/menu');
 app.use('/menu', menuController);
 
-// =======================
-//     DATABASE
-// =======================
+//___________________
+//Database
+//___________________
+// How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI;
 
-// Database Connection
-mongoose.connect(process.env.DATABASE_URL, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
-});
+// Connect to Mongo &
+// Fix Depreciation Warnings from Mongoose
+// May or may not need these depending on your Mongoose version
+mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
 
 
 // Database Connection Error/Success
